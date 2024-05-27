2889801faf6bc533eb97a4c5825b27079e05d087
Mike Pottebaum <michael.pottebaum@cdw.com>
2024-05-26 22:23:01 -0500

    fixed up the tutorialz

--- README.md
+++ README.md
@@ -2,7 +2,7 @@
 
 tha main cat man is back in town + the main cat town is overriden by wolves man
 
-![grandmaster flash](/assets/grandmaster-F.jpg)
+![grandmaster flash](/assets/grandmaster-F.jpeg)
 
 ## THA ART YO
 
@@ -13,4 +13,12 @@ Artists
 ## THA STORY YO
 
 GRANDMA$TER CA$H returns victorious from COMBAT MOUNTAIN only to find his hometown NAP NAP CITY overrun by THE WOLVES OF WACKNESS
+
+## THA TECH YO
+
+- [phaser](https://phaser.io/)
+
+### DEVO
+
+local runs on `localhost:3000`
 
--- gmc.go
+++ gmc.go
@@ -8,6 +8,7 @@ import (
 )
 
 var tmp *template.Template
+var PORT_STR = ":3000"
 
 func loadTemplate() {
 	t, err := template.ParseFiles("index.html")
@@ -23,11 +24,19 @@ func rootHandler(writer http.ResponseWriter, request *http.Request) {
 
 func main() {
 	loadTemplate()
+	http.Handle(
+		"/assets/",
+		http.StripPrefix(
+			"/assets/",
+			http.FileServer(http.Dir("./assets/")),
+		),
+	)
 	http.HandleFunc("/", rootHandler)
 
-	err := http.ListenAndServe(":3000", nil)
+	fmt.Printf("we up on port 3stax (%s)\n\n", PORT_STR)
+	err := http.ListenAndServe(PORT_STR, nil)
 	if err != nil {
 		log.Fatal(err)
+		fmt.Printf("err errywerr bruh: %s", err.Error())
 	}
-	fmt.Println("we up on port 3stax")
 }
